951d808850e89f3ba3f98c5d2a08b6d31205cd70
Sokol Misha <sokol.misha.m.com>
2022-10-02 15:46:56 +0300

    Connect to a PostgreSQL database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
 
-@Module({})
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      define: {
+        timestamps: false,
+      },
+      autoLoadModels: true,
+    }),
+  ],
+})
 export class AppModule {}
